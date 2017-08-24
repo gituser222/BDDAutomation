19ae1a6c60c37c12c1bad307dc6d557e03258c6f
damodaran <g.damodaran26@gmail.com>
2017-08-24 22:23:24 +1000

    New test.feature file added

--- target/Destination/report.js
+++ target/Destination/report.js
@@ -69,7 +69,7 @@ formatter.examples({
       "cells": [
         "180",
         "66",
-        "\"Normal1\""
+        "\"Normal\""
       ],
       "line": 14,
       "id": "bmi-feature;testing-of-bmi-page;;2"
@@ -133,7 +133,7 @@ formatter.step({
 });
 formatter.step({
   "line": 10,
-  "name": "Category is displayed in \"Normal1\"",
+  "name": "Category is displayed in \"Normal\"",
   "matchedColumns": [
     2
   ],
@@ -143,7 +143,7 @@ formatter.match({
   "location": "BMIPageSteps.user_is_on_BMI_page()"
 });
 formatter.result({
-  "duration": 6299146944,
+  "duration": 6771332414,
   "status": "passed"
 });
 formatter.match({
@@ -156,7 +156,7 @@ formatter.match({
   "location": "BMIPageSteps.user_enters_in_centimeters(String)"
 });
 formatter.result({
-  "duration": 117989354,
+  "duration": 172138125,
   "status": "passed"
 });
 formatter.match({
@@ -167,160 +167,4 @@ formatter.match({
     }
   ],
   "location": "BMIPageSteps.user_enters_in_Kilos(String)"
-});
-formatter.result({
-  "duration": 75394126,
-  "status": "passed"
-});
-formatter.match({
-  "location": "BMIPageSteps.user_clicks_on_Calculate_button()"
-});
-formatter.result({
-  "duration": 83419153,
-  "status": "passed"
-});
-formatter.match({
-  "location": "BMIPageSteps.BMI_is_displayed_is_BMI()"
-});
-formatter.result({
-  "duration": 44795,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "Normal1",
-      "offset": 26
-    }
-  ],
-  "location": "BMIPageSteps.Category_is_displayed_in(String)"
-});
-formatter.result({
-  "duration": 35703712,
-  "error_message": "org.junit.ComparisonFailure: expected:\u003cNormal[1]\u003e but was:\u003cNormal[]\u003e\r\n\tat org.junit.Assert.assertEquals(Assert.java:115)\r\n\tat org.junit.Assert.assertEquals(Assert.java:144)\r\n\tat runnerPackage.BMIPageSteps.Category_is_displayed_in(BMIPageSteps.java:69)\r\n\tat ✽.Then Category is displayed in \"Normal1\"(BMIPage.feature:10)\r\n",
-  "status": "failed"
-});
-formatter.write("Testing of BMI pagefailed");
-formatter.embedding("image/png", "embedded0.png");
-formatter.after({
-  "duration": 500862612,
-  "status": "passed"
-});
-formatter.scenario({
-  "line": 15,
-  "name": "Testing of BMI page",
-  "description": "",
-  "id": "bmi-feature;testing-of-bmi-page;;3",
-  "type": "scenario",
-  "keyword": "Scenario Outline",
-  "tags": [
-    {
-      "line": 3,
-      "name": "@regression"
-    }
-  ]
-});
-formatter.step({
-  "line": 5,
-  "name": "user is on BMI page",
-  "keyword": "Given "
-});
-formatter.step({
-  "line": 6,
-  "name": "user enters 160 in centimeters",
-  "matchedColumns": [
-    0
-  ],
-  "keyword": "When "
-});
-formatter.step({
-  "line": 7,
-  "name": "user enters 80 in Kilos",
-  "matchedColumns": [
-    1
-  ],
-  "keyword": "And "
-});
-formatter.step({
-  "line": 8,
-  "name": "user clicks on Calculate button",
-  "keyword": "And "
-});
-formatter.step({
-  "line": 9,
-  "name": "BMI is displayed is BMI",
-  "keyword": "Then "
-});
-formatter.step({
-  "line": 10,
-  "name": "Category is displayed in \"Obesity\"",
-  "matchedColumns": [
-    2
-  ],
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "BMIPageSteps.user_is_on_BMI_page()"
-});
-formatter.result({
-  "duration": 5335594243,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "160",
-      "offset": 12
-    }
-  ],
-  "location": "BMIPageSteps.user_enters_in_centimeters(String)"
-});
-formatter.result({
-  "duration": 121493130,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "80",
-      "offset": 12
-    }
-  ],
-  "location": "BMIPageSteps.user_enters_in_Kilos(String)"
-});
-formatter.result({
-  "duration": 76337796,
-  "status": "passed"
-});
-formatter.match({
-  "location": "BMIPageSteps.user_clicks_on_Calculate_button()"
-});
-formatter.result({
-  "duration": 81425589,
-  "status": "passed"
-});
-formatter.match({
-  "location": "BMIPageSteps.BMI_is_displayed_is_BMI()"
-});
-formatter.result({
-  "duration": 16638,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "Obesity",
-      "offset": 26
-    }
-  ],
-  "location": "BMIPageSteps.Category_is_displayed_in(String)"
-});
-formatter.result({
-  "duration": 33934972,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 75698303,
-  "status": "passed"
-});
 });
